ea2a7eec9dd973cb898910e0130b26a198e2f03f
Valeria Figueredo <valeriafigueredo395@gmail.com>
2025-12-08 16:16:08 -0300

    reversion en vite

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '', // ✔ Railway sirve desde raíz del dominio
-
+    base: '',
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: false, // ❗ evita que intente modo HMR en producción
+            refresh: true,
         }),
     ],
-
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
 });
-
